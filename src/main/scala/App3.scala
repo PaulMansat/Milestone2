dab4ecb4fd0fc07708d6f662c7a94bc69e0a8dad
PaulMansat <paul.mansat@epfl.ch>
2020-04-04 20:56:15 +0200

    Last updates on App3 and App5. Comments were added. Ready for handout

--- src/main/scala/App3.scala
+++ src/main/scala/App3.scala
@@ -19,14 +19,19 @@ object App3 {
 
       (k, v)
     })
-    val sum = rows.groupBy(_._1).map { case (k, l) => k -> l.map(_._2).sum }
-    val extendedSum = sum.map{ case (x) => x._1 * (2 to 100).foldLeft(0){case (acc, i) => acc + x._2 % i}}.collect
 
     /*
-    val extendedSum = sum.flatMap{ case (x) => (2 to 100).map(i => x._1 -> x._2 % i)}.collect
-    */
-    //sextendedSum.foreach{case (x1, x2) => println("1: " + x1 + " 2: " + x2)}
-    val res = extendedSum.sum
+    Original code:
+      val sum = rows.groupBy(_._1).map { case (k, l) => k -> l.map(_._2).sum }.collect
+      val extendedSum = sum.flatMap(x => (2 to 100).map(i => x._1 -> x._2 % i))
+      val res = extendedSum.foldLeft(0){case (acc, cur) => acc + cur._1 * cur._2}
+    We now update the code according to the solution described in the report (i.e reduce the size of the intermediate
+    data-set extendedSum by doing operation on-the-fly).
+     */
+    val sum = rows.groupBy(_._1).map { case (k, l) => k -> l.map(_._2).sum }
+    val extendedSum = sum.map{ case (x) => x._1 * (2 to 100).foldLeft(0){case (acc, i) => acc + x._2 % i}}
+
+    val res = extendedSum.collect.sum
     println("Result = " + res)
   }
 }
--- src/main/scala/App5.scala
+++ src/main/scala/App5.scala
@@ -19,7 +19,21 @@ object App5 {
       (k, v)
     })
 
-    val sum = rows.groupBy(_._1).map{case (z) => z._1 -> z._2.foldLeft(0){case(acc, n) => acc + (2 to 50).foldLeft(0){case (acc2, i) => acc2 + (n._2 % i)}}}
+    /*
+    Original code:
+      val extendedRows = rows.mapPartitions(it  => {
+      val l = it.toList
+      val l2 = l.flatMap(x => (2 to 50).map(i => (x._1, x._2 % i)))
+      l2.iterator
+      })
+      val sum = extendedRows.groupBy(_._1).map { case (k, l) => k -> l.map(_._2).sum}
+    We now update the code according to the solution described in the report (i.e reduce the size of the intermediate
+    data-set extendedSum by doing operation on-the-fly). To do so, we first the data structure by key and apply a
+    map-foldleft to do the sum of the modulo of each elements
+     */
+
+    val sum = rows.groupBy(_._1).map{case (k, l) => k -> l.map{case (_, n) => (2 to 50).foldLeft(0){case (acc, i) => acc + (n%i)}}.sum}
+    //val sum = rows.groupBy(_._1).map{case (z) => z._1 -> z._2.foldLeft(0){case(acc, n) => acc + (2 to 50).foldLeft(0){case (acc2, i) => acc2 + (n._2 % i)}}}
     sum.collect().foreach{case (x1, x2) => println("1: " + x1 + " 2: " + x2)}
 
   }
